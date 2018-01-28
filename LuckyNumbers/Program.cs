ef12f1c862adab3596b8acbafc6361351c0c718d
NTorres67 <ntorres67@sbcglobal.net>
2018-01-27 23:34:57 -0500

    Determined of numbers user gussed correctly and jacpot amount

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -10,53 +10,57 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
-            double startNum = 0;
-            double endNum = 0;
-            double jackpot = 10000;
+            int startNumber = 0;
+            int endNumber = 0;
+            int jackpot = 10000;
             string playGame;
-            //double gameNum = 0;
-            //double array = 0;
-            //int numGood = 0;
-            //int numBad = 0;
+            int indexes = 6;
+            
 
-            Console.WriteLine("Welcome to We Can Code IT's Lucky Number game. Today's winning amount for the 6 number jackpot is $" + jackpot + "!");
-           
+
             //Introduce user to the game and advise user of jackpot ammount
             //Determine if user wants to exit the application.
             //Have user enter number indicating the lowest and highest values for the number range
+
+            Console.WriteLine("Welcome to We Can Code IT's Lucky Number game. Today's winning amount for the 6 number jackpot is $" + jackpot + "!");
+            Console.WriteLine("");
             Console.WriteLine("Do you wish to play? Please indicate yes or no.");
             playGame = Console.ReadLine();
             string play = playGame.ToLower();
+
             if (playGame == "yes")
             {
                 Console.WriteLine("Please enter the smallest number you will selecting from to create your number range.");
-                startNum = int.Parse(Console.ReadLine());
+                startNumber = int.Parse(Console.ReadLine());
                 //Console.WriteLine(startNum);
+                Console.WriteLine("");
 
                 Console.WriteLine("Please enter the largest number you will be selecting from to create your number range.");
-                endNum = int.Parse(Console.ReadLine());
+                endNumber = int.Parse(Console.ReadLine());
                 //Console.WriteLine(endNum);
+                Console.WriteLine("");
             }
-            if (playGame == "")
+            if (playGame == "no")
             {
                 Environment.Exit(0);
             }
 
-            // Create an array with a user determined number range.
-            // Validate user input. If user input is not within user determined range,
-            // display the message "Invalid Input". Keep looping until user enters 6  values
-            // within specified number range.
 
-            double[] userInput = new double[6];
+            // Create an array with a user determined number range.
+
+            int[] userInput = new int[indexes];
+
             for (int i = 0; i < userInput.Length; i++)
             {
-                //int counterNumber = 0;
-                Console.WriteLine("Enter a number between " + startNum + " and " + endNum + ".");
+                Console.WriteLine("Enter a number between " + startNumber + " and " + endNumber + ".");
                 userInput[i] = int.Parse(Console.ReadLine());
-                //Console.WriteLine(nArray[i]);
                 Console.WriteLine("");
 
-                if ((userInput[i] >= startNum && userInput[i] <= endNum))
+                // Validate user input. If user input is not within user determined range,
+                // display the message "Invalid Input". Keep looping until user enters 6  values
+                // within specified number range. 
+                
+                if (userInput[i] >= startNumber && userInput[i] <= endNumber)
                 {
                     i = i++;
                 }
@@ -65,9 +69,49 @@ namespace LuckyNumbers
                     i = i - 1;
                     Console.WriteLine("Invalid Input.");
                 }
+
             }
             
 
+
+            // Randomly generate 6 numbers using a for loop 
+
+            Random r = new Random();
+            int[] randomArray = new int[indexes];
+            for (int z = 0; z < randomArray.Length; z++)
+            {
+                randomArray[z] = r.Next(startNumber, endNumber);
+                Console.WriteLine("");
+                Console.WriteLine("Lucky Number: " + randomArray[z]);
+                Console.WriteLine("");
+            }
+
+
+            // Compare user input numbers to randomly genterated numbers
+            // Determine how many user input numbers are the same as the 
+            //randomly generated numbers
+
+            int numbersSame = 0;
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                for (int z = 0; z < randomArray.Length; z++)
+                {
+                    if (userInput[i] == randomArray[z]) 
+                        
+                    numbersSame++;
+                    z = z++;
+                   
+                }
+            }
+            Console.WriteLine("You guessed " + numbersSame + " numbers correctly!");
+            Console.WriteLine("");
+
+
+            // Determine amount of jackpot won
+
+            int winnings = 0;
+            winnings = jackpot / indexes * numbersSame;
+            Console.WriteLine("You won $" + winnings + "!");
 
         }
     }
